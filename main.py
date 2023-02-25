218eb816c6ecbee964f2b6ae722f20f4b1d6bc3f
piotex <pkubon2@gmail.com>
2023-02-25 22:15:34 +0100

    skeleton

--- main.py
+++ main.py
@@ -1,52 +1,164 @@
-from __future__ import print_function
-
 import datetime
 import os.path
 
+import googleapiclient
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+service: googleapiclient.discovery.Resource = None
+time_zone_eu = "Europe/Warsaw"
+tz_parser = '%Y-%m-%dT%H:%M:%S+01:00'
+zero_datatime: datetime.timedelta = None
 
 
-def main():
+def init():
+    global zero_datatime
+    zero_datatime = datetime.datetime.strptime("0:0:0", "%H:%M:%S")
+    zero_datatime = datetime.timedelta(hours=zero_datatime.hour, minutes=zero_datatime.minute,
+                                       seconds=zero_datatime.second)
+
+
+def get_create_creds():
+    scopes = ['https://www.googleapis.com/auth/calendar']
     creds = None
     if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        creds = Credentials.from_authorized_user_file('token.json', scopes)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', scopes)
             creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+    return creds
 
-    try:
+
+def init_service(creds) -> googleapiclient.discovery.Resource:
+    global service
+    if service is None:
         service = build('calendar', 'v3', credentials=creds)
+    return service
 
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
 
-        if not events:
-            print('No upcoming events found.')
-            return
+def get_events():
+    global service, time_zone_eu
+    day_start = datetime.datetime.now().strftime("%Y-%m-%dT0:0:0Z")  # 'Z' indicates UTC time
+    day_end_t = datetime.datetime.now().strftime("%Y-%m-%dT23:59:59Z")  # 'Z' indicates UTC time
 
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
+    events_result = service.events().list(calendarId='primary', timeMin=day_start,
+                                          timeMax=day_end_t, singleEvents=True,
+                                          orderBy='startTime', timeZone=time_zone_eu).execute()
 
-    except HttpError as error:
-        print('An error occurred: %s' % error)
+    events = events_result.get('items', [])
+    return events
+
+
+def insert_event(break_time, start, end_t):
+    global service, time_zone_eu
+    event = {
+        "colorId": "8",
+        'summary': f'--break: {break_time}',
+        'start': {
+            'dateTime': start,
+            'timeZone': time_zone_eu,
+        },
+        'end': {
+            'dateTime': end_t,
+            'timeZone': time_zone_eu,
+        }
+    }
+    event = service.events().insert(calendarId='primary', body=event).execute()
+
+
+def get_first_ok_i(events):
+    global service, time_zone_eu, tz_parser, zero_datatime
+
+    does_except = False
+    first_ok_i = 0
+
+    for i in range(0, len(events)):
+        try:
+            event = events[i]
+            end_t = event['start'].get('dateTime')
+            datetime.datetime.strptime(end_t, tz_parser)
+            if does_except:
+                first_ok_i = i
+                does_except = False
+        except:
+            does_except = True
+            continue
+    return first_ok_i
+
+
+def add_breaks_time(events):
+    counter_datatime = zero_datatime
+    start = events[0]['end'].get('dateTime')
+    first_ok_i = get_first_ok_i(events)
+
+    for i in range(first_ok_i, len(events)):
+        event = events[i]
+        end_t = event['start'].get('dateTime')
+        try:
+            a1 = datetime.datetime.strptime(end_t, tz_parser)
+            a2 = datetime.datetime.strptime(start, tz_parser)
+            break_time = a1 - a2
+            if break_time > zero_datatime:
+                counter_datatime += break_time
+                insert_event(break_time, start, end_t)
+                print(break_time, start, end_t)
+
+        except Exception as eee:
+            print(eee)
+            continue
+
+        event = events[i]
+        start = event['end'].get('dateTime')
+
+
+def add_total_break_time(events):
+    global service, time_zone_eu, tz_parser, zero_datatime
+    counter_datatime = zero_datatime
+    start = events[0]['end'].get('dateTime')
+    first_ok_i = get_first_ok_i(events)
+
+    for i in range(first_ok_i, len(events)):
+        event = events[i]
+        end_t = event['start'].get('dateTime')
+        try:
+            a1 = datetime.datetime.strptime(end_t, tz_parser)
+            a2 = datetime.datetime.strptime(start, tz_parser)
+            break_time = a1 - a2
+            if break_time > zero_datatime:
+                counter_datatime += break_time
+
+        except Exception as eee:
+            print(eee)
+            continue
+
+        event = events[i]
+        start = event['end'].get('dateTime')
+
+    time_format = "%Y-%m-%dT"
+    day_start = datetime.datetime.now().strftime(time_format)  # 'Z' indicates UTC time
+    print(counter_datatime)
+
+    insert_event(counter_datatime,
+                 datetime.datetime.strptime(day_start, time_format).strftime(tz_parser),
+                 (datetime.datetime.strptime(day_start, time_format) + datetime.timedelta(days=1)).strftime(tz_parser))
+
+
+
+def main():
+    global service
+    init()
+    creds = get_create_creds()
+    init_service(creds)
+    events = get_events()
+    add_breaks_time(events)
+    add_total_break_time(events)
 
 
 if __name__ == '__main__':
